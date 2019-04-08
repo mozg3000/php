ea8ef32e1ef5b4bc447a1d17ac5b0b14834e2ce0
mozg3000 <i.luka@outlook.com>
2019-04-08 10:45:59 +0300

    init

---
+++ localhost/main.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h2>
+    Это основной шаблон >>> start
+</h2>
+<?=$content?>
+<p>
+    Это основной шаблон >>> end
+</p>
+</body>
+</html>
