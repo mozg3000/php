87ae5cfd8ddf9fa9bec5227a7511397786e9992c
mozg3000 <i.luka@outlook.com>
2019-04-15 11:40:14 +0300

    hw4 with fileUpload function

--- localhost/upload.php
+++ localhost/upload.php
@@ -1,50 +1,52 @@
-<html>
-<meta charset="UTF-8"> 
-<link rel="stylesheet" type="text/css" href="/datepicker.css" />
-	<title>Результат загрузки файла</title>  
-  </head>
-<body>
 <?php
- $uploaddir = 'images/foto/'; // Relative path under webroot
- $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+function uploadFile($iname, $path)
+{
+    $flag = true;
 //Проверка существует ли файл
-if (file_exists($uploadfile)) { echo "Файл $uploadfile существует, выберите другое имя файла!"; exit;} 
+    if (file_exists($path)) {
+        echo "Файл $path существует, выберите другое имя файла!";
+        $flage = false;
+        exit;
+    }
 
-//Проверка на размер файла 
-   if($_FILES["userfile"]["size"] > 1024*1*1024)
-   {
-     echo ("Размер файла не больше 5 мб");
-     exit;
-   }
+//Проверка на размер файла
+    if ($_FILES[$iname]["size"] > 1024 * 1 * 1024) {
+        echo("Размер файла не больше 5 мб");
+        $flage = false;
+        exit;
+    }
 //Проверка расширения файла
-$blacklist = array(".php", ".phtml", ".php3", ".php4");
- foreach ($blacklist as $item) {
-  if(preg_match("/$item\$/i", $_FILES['userfile']['name'])) {
-   echo "Загрузка php-файлов запрещена!";
-   exit;
-   }
-  }
+    $blacklist = array(".php", ".phtml", ".php3", ".php4");
+    foreach ($blacklist as $item) {
+        if (preg_match("/$item\$/i", $_FILES[$iname]['name'])) {
+            echo "Загрузка php-файлов запрещена!";
+            $flage = false;
+            exit;
+        }
+    }
 //Проверка на тип файла
-$imageinfo = getimagesize($_FILES['userfile']['tmp_name']);
- if($imageinfo['mime'] != 'image/gif' && $imageinfo['mime'] != 'image/jpeg') {
-  echo "Можно загружать только jpg-файлы, неверное содержание файла, не изображение.";
-  exit;
- }
 
- if($_FILES['userfile']['type'] != "image/jpeg") {
-   echo "Можно загружать только jpg-файлы.";
-   exit;
- }
- 
- if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-   echo "Файл успешно загружен.\n";
- } else {
-   echo "Загрузка не получилась.\n";
- }
-?>
-<br><br>
+    $imageinfo = getimagesize($_FILES[$iname]['tmp_name']);
+    if ($imageinfo['mime'] != 'image/gif' && $imageinfo['mime'] != 'image/jpeg') {
+        echo "Можно загружать только jpg-файлы, неверное содержание файла, не изображение.";
+        $flage = false;
+        exit;
+    }
 
-</body>
-</html>
+    if ($_FILES_m[$iname]['type'] != "image/jpeg") {
+        echo "Можно загружать только jpg-файлы.";
+        $flage = false;
+        exit;
+    }
+
+    if($flag){
+        if (move_uploaded_file($_FILES[$iname]['tmp_name'], $path)) {
+            echo "ФАйл загружен<br/>";
+        } else {
+
+            echo "Error <br/>";
+        }
+    }
+}
 
 
