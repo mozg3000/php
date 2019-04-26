0be7036555262eaaf1dc26b58019083237453d8b
mozg3000 <i.luka@outlook.com>
2019-04-26 13:10:46 +0300

    hw6 with feedback

--- engino/data/enginodb.sql
+++ engino/data/enginodb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 22 2019 г., 14:31
+-- Время создания: Апр 26 2019 г., 13:09
 -- Версия сервера: 5.7.25
 -- Версия PHP: 7.1.22
 
@@ -25,6 +25,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `cart`
+--
+
+CREATE TABLE `cart` (
+  `id_cart` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `id_session` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `cart`
+--
+
+INSERT INTO `cart` (`id_cart`, `id_product`, `id_session`) VALUES
+(1, 2, 'vmhoqalojeadgf8kt6ossc5bkrjdgf6p'),
+(2, 4, 'vmhoqalojeadgf8kt6ossc5bkrjdgf6p'),
+(3, 9, 'vmhoqalojeadgf8kt6ossc5bkrjdgf6p'),
+(4, 12, 'vmhoqalojeadgf8kt6ossc5bkrjdgf6p'),
+(14, 14, 'vmhoqalojeadgf8kt6ossc5bkrjdgf6p'),
+(15, 13, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(17, 3, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(18, 3, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(19, 9, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(20, 9, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(22, 5, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(23, 5, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(24, 5, 'asjqf2vqni8dl9oj67323rudiq1s5669'),
+(25, 10, 'mgc82nv3gf3iu4cou9in6v7g78u2k14a'),
+(26, 11, 'mgc82nv3gf3iu4cou9in6v7g78u2k14a'),
+(27, 9, 'mgc82nv3gf3iu4cou9in6v7g78u2k14a'),
+(28, 3, 'mgc82nv3gf3iu4cou9in6v7g78u2k14a'),
+(29, 7, 'mgc82nv3gf3iu4cou9in6v7g78u2k14a'),
+(30, 12, 'vqr4428orov91oa142koptnj72lmvm37'),
+(31, 10, 'vqr4428orov91oa142koptnj72lmvm37'),
+(32, 9, 'vqr4428orov91oa142koptnj72lmvm37'),
+(33, 7, 'vqr4428orov91oa142koptnj72lmvm37'),
+(34, 4, 'vqr4428orov91oa142koptnj72lmvm37'),
+(35, 12, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(36, 12, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(37, 12, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(38, 12, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(39, 10, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(40, 10, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(41, 10, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(42, 5, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(43, 5, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(44, 5, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(45, 15, 'n84kjnfi7svndnulnrm7o98lie8plo37'),
+(46, 15, 'n84kjnfi7svndnulnrm7o98lie8plo37');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `catalog`
 --
 
@@ -32,6 +85,7 @@ CREATE TABLE `catalog` (
   `id` int(11) NOT NULL,
   `product_name` text NOT NULL,
   `description` text,
+  `price` float NOT NULL,
   `img` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -39,30 +93,30 @@ CREATE TABLE `catalog` (
 -- Дамп данных таблицы `catalog`
 --
 
-INSERT INTO `catalog` (`id`, `product_name`, `description`, `img`) VALUES
-(1, 'Товар1', 'Описание товара1', '01.jpg'),
-(2, 'Товар2', 'Описание товара2', '02.jpg'),
-(3, 'Товар3', 'Описание товара3', '03.jpg'),
-(4, 'Товар4', 'Описание товара4', '04.jpg'),
-(5, 'Товар5', 'Описание товара5', '05.jpg'),
-(6, 'Товар6', 'Описание товара6', '06.jpg'),
-(7, 'Товар7', 'Описание товара7', '07.jpg'),
-(8, 'Товар8', 'Описание товара8', '08.jpg'),
-(9, 'Товар9', 'Описание товара9', '09.jpg'),
-(10, 'Товар10', 'Описание товара10', '10.jpg'),
-(11, 'Товар11', 'Описание товара11', '11.jpg'),
-(12, 'Товар12', 'Описание товара12', '12.jpg'),
-(13, 'Товар13', 'Описание товара13', '13.jpg'),
-(14, 'Товар14', 'Описание товара14', '14.jpg'),
-(15, 'Товар15', 'Описание товара15', '15.jpg');
+INSERT INTO `catalog` (`id`, `product_name`, `description`, `price`, `img`) VALUES
+(1, 'Товар1', 'Описание товара1', 100, '01.jpg'),
+(2, 'Товар2', 'Описание товара2', 200, '02.jpg'),
+(3, 'Товар3', 'Описание товара3', 300, '03.jpg'),
+(4, 'Товар4', 'Описание товара4', 400, '04.jpg'),
+(5, 'Товар5', 'Описание товара5', 500, '05.jpg'),
+(6, 'Товар6', 'Описание товара6', 400, '06.jpg'),
+(7, 'Товар7', 'Описание товара7', 300, '07.jpg'),
+(8, 'Товар8', 'Описание товара8', 100, '08.jpg'),
+(9, 'Товар9', 'Описание товара9', 1000, '09.jpg'),
+(10, 'Товар10', 'Описание товара10', 1500, '10.jpg'),
+(11, 'Товар11', 'Описание товара11', 1200, '11.jpg'),
+(12, 'Товар12', 'Описание товара12', 1600, '12.jpg'),
+(13, 'Товар13', 'Описание товара13', 1800, '13.jpg'),
+(14, 'Товар14', 'Описание товара14', 900, '14.jpg'),
+(15, 'Товар15', 'Описание товара15', 800, '15.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `feedbacks`
+-- Структура таблицы `feedback`
 --
 
-CREATE TABLE `feedbacks` (
+CREATE TABLE `feedback` (
   `id` int(11) NOT NULL,
   `sender` text NOT NULL,
   `message` text NOT NULL,
@@ -70,10 +124,10 @@ CREATE TABLE `feedbacks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `feedbacks`
+-- Дамп данных таблицы `feedback`
 --
 
-INSERT INTO `feedbacks` (`id`, `sender`, `message`, `product_id`) VALUES
+INSERT INTO `feedback` (`id`, `sender`, `message`, `product_id`) VALUES
 (1, 'vasya', 'jkelasrhgkljherwkjs', 1),
 (2, 'ghfhgf', '    fdxfdxfdxdf', 10),
 (3, 'ghfhgf', '    gvhvhgvh', 10),
@@ -96,172 +150,52 @@ INSERT INTO `feedbacks` (`id`, `sender`, `message`, `product_id`) VALUES
 (20, 'vhgvgh', '    vhgvhgvhgvhg', 10),
 (21, 'bjhjhbjh', '    bjhbjhbjhbjhb', 0),
 (22, 'bjhjhbjh', '    bjhbjhbjhbjhb', 0),
-(23, '', '', 0),
-(24, '', '', 0),
-(25, '', '', 0),
-(26, '', '', 0),
-(27, '', '', 0),
-(28, '', '', 0),
-(29, '', '', 0),
-(30, '', '', 0),
-(31, '', '', 0),
-(32, '', '', 0),
-(33, '', '', 0),
-(34, '', '', 0),
-(35, '', '', 0),
-(36, '', '', 0),
-(37, '', '', 0),
-(38, '', '', 0),
-(39, '', '', 0),
-(40, '', '', 0),
-(41, '', '', 0),
 (42, 'hgjhgjhgj', '    gjhgjhgjgjhgjhgjh', 2),
-(43, '', '', 0),
-(44, '', '', 0),
-(45, '', '', 0),
-(46, '', '', 0),
-(47, '', '', 0),
-(48, '', '', 0),
-(49, '', '', 0),
-(50, '', '', 0),
-(51, '', '', 0),
-(52, '', '', 0),
-(53, '', '', 0),
-(54, '', '', 0),
-(55, '', '', 0),
-(56, '', '', 0),
-(57, '', '', 0),
-(58, '', '', 0),
-(59, '', '', 0),
-(60, '', '', 0),
-(61, '', '', 0),
-(62, '', '', 0),
-(63, '', '', 0),
-(64, '', '', 0),
-(65, '', '', 0),
-(66, '', '', 0),
-(67, '', '', 0),
-(68, '', '', 0),
-(69, '', '', 0),
-(70, '', '', 0),
-(71, '', '', 0),
-(72, '', '', 0),
-(73, '', '', 0),
-(74, '', '', 0),
-(75, '', '', 0),
-(76, '', '', 0),
-(77, '', '', 0),
-(78, '', '', 0),
-(79, '', '', 0),
-(80, '', '', 0),
-(81, '', '', 0),
-(82, '', '', 0),
-(83, '', '', 0),
-(84, '', '', 0),
-(85, '', '', 0),
-(86, '', '', 0),
-(87, '', '', 0),
-(88, '', '', 0),
-(89, '', '', 0),
-(90, '', '', 0),
-(91, '', '', 0),
-(92, '', '', 0),
-(93, '', '', 0),
-(94, '', '', 0),
-(95, '', '', 0),
-(96, '', '', 0),
-(97, '', '', 0),
-(98, '', '', 0),
-(99, '', '', 0),
-(100, '', '', 0),
-(101, '', '', 0),
-(102, '', '', 0),
-(103, '', '', 0),
-(104, '', '', 0),
-(105, '', '', 0),
-(106, '', '', 0),
-(107, '', '', 0),
-(108, '', '', 0),
-(109, '', '', 0),
-(110, '', '', 0),
-(111, '', '', 0),
-(112, '', '', 0),
-(113, '', '', 0),
-(114, '', '', 0),
-(115, '', '', 0),
-(116, '', '', 0),
-(117, '', '', 0),
-(118, '', '', 0),
-(119, '', '', 0),
-(120, '', '', 0),
-(121, '', '', 0),
-(122, '', '', 0),
-(123, '', '', 0),
-(124, '', '', 0),
-(125, '', '', 0),
-(126, '', '', 0),
-(127, '', '', 0),
-(128, '', '', 0),
-(129, '', '', 0),
-(130, '', '', 0),
-(131, '', '', 0),
-(132, '', '', 0),
-(133, '', '', 0),
-(134, '', '', 0),
-(135, '', '', 0),
-(136, '', '', 0),
-(137, '', '', 0),
-(138, '', '', 0),
-(139, '', '', 0),
-(140, '', '', 0),
-(141, '', '', 0),
-(142, '', '    ', 3),
-(143, '', '', 0),
-(144, '', '', 0),
-(145, '', '', 0),
-(146, '', '', 0),
-(147, '', '', 0),
-(148, '', '', 0),
-(149, '', '', 0),
-(150, '', '', 0),
-(151, '', '', 0),
-(152, '', '', 0),
-(153, '', '', 0),
-(154, '', '', 0),
-(155, '', '', 0),
-(156, '', '', 0),
-(157, '', '', 0),
-(158, '', '', 0),
-(159, '', '', 0),
-(160, '', '', 0),
-(161, '', '', 0),
-(162, '', '', 0),
-(163, '', '', 0),
 (164, 'vhgvhg', '    vvhgvhgvhg', 2),
 (165, 'hkjhk', '    jhkhkjhk', 2),
-(166, '', '', 0),
-(167, '', '', 0),
-(168, '', '', 0),
-(169, '', '', 0),
-(170, '', '', 0),
-(171, '', '', 0),
-(172, '', '', 0),
-(173, '', '', 0),
-(174, '', '', 0),
-(175, '', '', 0),
-(176, '', '', 0),
-(177, '', '', 0),
-(178, '', '', 0),
-(179, '', '', 0),
-(180, '', '', 0),
-(181, '', '', 0),
-(182, '', '', 0),
-(183, '', '', 0),
-(184, '', '', 0),
 (185, 'jkjbkj', '    bkjbkjbkj', 2),
 (186, 'llllllllllllllllllllllll', '    llllkkkkkkkkkkkkkkkkkkkkk', 2),
 (187, 'ghfhgf', '    jhkjhkjhkjhkj', 2),
-(188, 'hhhhhhhhhhhhhhh', '    hhhhhhhhhhhhhhhhhhhhhhhhhh', 2);
+(188, 'hhhhhhhhhhhhhhh', '    hhhhhhhhhhhhhhhhhhhhhhhhhh', 2),
+(189, 'oracle', 'on my opinion ', 7),
+(190, 'oracle', '    hjjhbjhbjhbj', 2),
+(191, 'oracle', '    45465454', 3),
+(192, 'oracle', '    654654654', 7),
+(193, 'oracle', '   454646 ', 7),
+(194, 'oracle', '   454646 ', 7),
+(195, 'oracle', '    jjjjjjjjjjjjjjjjjjjjjj', 7),
+(196, 'oracle', '    ttttttttttttttttt', 15),
+(197, 'oracle', '    kokokokokoko', 0),
+(198, 'oracle', '    555555555555555555555', 0),
+(199, 'oracle', '    5555555555555555555', 15),
+(200, '1', '    11111111111111111', 15),
+(201, '1', '    1111111111111', 15),
+(202, 'oracle', '    uuuuuuuuuuuuuuuuuuuuu', 15),
+(203, 'oracle', '    000000000000000000', 15);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id_order` int(11) NOT NULL,
+  `id_session` varchar(32) NOT NULL,
+  `telefon` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id_order`, `id_session`, `telefon`) VALUES
+(2, 'vmhoqalojeadgf8kt6ossc5bkrjdgf6p', 587585765),
+(3, 'asjqf2vqni8dl9oj67323rudiq1s5669', 6758765),
+(4, 'snu9q9nc73fapns73qlo789nq0ueh27m', 890809),
+(5, 'mgc82nv3gf3iu4cou9in6v7g78u2k14a', 587585765),
+(6, 'vqr4428orov91oa142koptnj72lmvm37', 8978675),
+(7, 'n84kjnfi7svndnulnrm7o98lie8plo37', 6758765);
 
 -- --------------------------------------------------------
 
@@ -315,9 +249,35 @@ INSERT INTO `picture_type` (`id`, `size`) VALUES
 (1, 'small'),
 (2, 'big');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` text NOT NULL,
+  `pass` text NOT NULL,
+  `hash` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`, `hash`) VALUES
+(1, 'admin', '$2y$10$GAh95KWqFf1Fw4YyH/BCnuODYbJ1Mln78vDuOIwj7WQvChhR8QcX.', '9453319475cc17b0c4ec214.41105699');
+
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id_cart`);
 
 --
 -- Индексы таблицы `catalog`
@@ -326,10 +286,16 @@ ALTER TABLE `catalog`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `feedbacks`
+-- Индексы таблицы `feedback`
 --
-ALTER TABLE `feedbacks`
+ALTER TABLE `feedback`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id_order`);
 
 --
 -- Индексы таблицы `pictures`
@@ -344,8 +310,20 @@ ALTER TABLE `picture_type`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT для таблицы `catalog`
@@ -354,10 +332,16 @@ ALTER TABLE `catalog`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT для таблицы `feedbacks`
+-- AUTO_INCREMENT для таблицы `feedback`
 --
-ALTER TABLE `feedbacks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=189;
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=204;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `pictures`
@@ -370,6 +354,12 @@ ALTER TABLE `pictures`
 --
 ALTER TABLE `picture_type`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
