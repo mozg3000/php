fc216e6ab64c9827b367a246ee3a950e15acabe0
mozg3000 <i.luka@outlook.com>
2019-05-01 10:47:33 +0300

    hw8 addProduct feature added

--- engino/views/product.php
+++ engino/views/product.php
@@ -1,48 +1,31 @@
 
 <?//= var_dump($edit);?>
 <div>
-    <?if(!$edit):?>
-    <p>
-        <a href="/product/edit/<?=$id?>">
-            Редактировать
-        </a>
-    </p>
-    <?else:?>
-        <p>
-            <a href="/product/<?=$id?>">
-                Отмена
-            </a>
-        </p>
-    <?endif;?>
+    <a href="#" id="edit">
+        Редактировать
+    </a>
+    <a href="#" id="cancel" style="display: none">
+        Отмена
+    </a>
+    <br>
     <img src="<?= GALLERY_DIR . 'big/' . $img?>" alt="" style="width: 350px"><br>
-<!--    --><?//= var_dump($_GET['img'],'tut');?>
-    <?if($edit):?>
-        <?if($_GET["new_img"]):?>
-            Имя файла:- <?=$img?><br>
-        <img src="<?=GALLERY_DIR . 'small/' . $_GET["new_img"]?>" alt=""><br>
-        <?else:?>
-<!--            --><?//= var_dump($_GET['img'],'zdes');?>
-            <form method="post" action="/product/edit/img/<?=$id?>" enctype="multipart/form-data">
+    <form method="post" action="/product/edit/img/<?=$id?>" enctype="multipart/form-data"
+            name="img_download" style="display: none">
 
-                Изображение <br>
-                <input type="file" name="rfile"><br>
-                <input type="submit" name="load" value="Загрузить"><br>
-            </form>
-        <?endif;?>
-    <?endif;?>
-    <form action="/product/edited/<?=$id?>" method="post">
-        <input type="text" name="name" value="<?=$product_name?>" <?if(!$edit){echo "readonly";}?>>
-        <br>
-        <input type="text" name="price" value="<?=$price?>" <?if(!$edit){echo "readonly";}?>>
-        <br>
-        <textarea name="description" cols="30" rows="10" <?if(!$edit){echo "readonly";}?>>
-        <?=$description?>
-    </textarea><br>
-        <?if($edit):?>
-        <input type="submit" name="edit" value="Изменить">
-        <?endif;?>
+        Изображение <br>
+        <input type="file" name="rfile"><br>
+        <input type="button" name="load" value="Загрузить"><br>
     </form>
-<!--    --><?//=$id?>
+    <form name="product" action="/product/edited/--><?=$id?><!--" method="post">
+        <input type="text" name="name" value="<?=$product_name?>" readonly>
+        <br>
+        <input type="text" name="price" value="<?=$price?>" readonly>
+        <br>
+        <textarea name="description" cols="30" rows="10" readonly>
+            <?=$description?>
+        </textarea><br>
+            <input type="submit" name="edit" value="Изменить" style="display: none">
+        </form>
 </div>
 <p>
     Добавте свой отзыв
@@ -68,3 +51,51 @@
 </p>
     <hr>
 <?endforeach;?>
+<script>
+    <?include "../public/js/product.js";?>
+</script>
+<!---->
+<!--<div>-->
+<!--    --><?//if(!$edit):?>
+<!--        <p>-->
+<!--            <a href="/product/edit/--><?//=$id?><!--">-->
+<!--                Редактировать-->
+<!--            </a>-->
+<!--        </p>-->
+<!--    --><?//else:?>
+<!--        <p>-->
+<!--            <a href="/product/--><?//=$id?><!--">-->
+<!--                Отмена-->
+<!--            </a>-->
+<!--        </p>-->
+<!--    --><?//endif;?>
+<!--    <img src="--><?//= GALLERY_DIR . 'big/' . $img?><!--" alt="" style="width: 350px"><br>-->
+<!--    <!--    -->--><?////= var_dump($_GET['img'],'tut');?>
+<!--    --><?//if($edit):?>
+<!--        --><?//if($_GET["new_img"]):?>
+<!--            Имя файла:- --><?//=$img?><!--<br>-->
+<!--            <img src="--><?//=GALLERY_DIR . 'small/' . $_GET["new_img"]?><!--" alt=""><br>-->
+<!--        --><?//else:?>
+<!--            <!--            -->--><?////= var_dump($_GET['img'],'zdes');?>
+<!--            <form method="post" action="/product/edit/img/--><?//=$id?><!--" enctype="multipart/form-data">-->
+<!---->
+<!--                Изображение <br>-->
+<!--                <input type="file" name="rfile"><br>-->
+<!--                <input type="submit" name="load" value="Загрузить"><br>-->
+<!--            </form>-->
+<!--        --><?//endif;?>
+<!--    --><?//endif;?>
+<!--    <form action="/product/edited/--><?//=$id?><!--" method="post">-->
+<!--        <input type="text" name="name" value="--><?//=$product_name?><!--" --><?//if(!$edit){echo "readonly";}?><!-->-->
+<!--        <br>-->
+<!--        <input type="text" name="price" value="--><?//=$price?><!--" --><?//if(!$edit){echo "readonly";}?><!-->-->
+<!--        <br>-->
+<!--        <textarea name="description" cols="30" rows="10" --><?//if(!$edit){echo "readonly";}?><!-->-->
+<!--        --><?//=$description?>
+<!--    </textarea><br>-->
+<!--        --><?//if($edit):?>
+<!--            <input type="submit" name="edit" value="Изменить">-->
+<!--        --><?//endif;?>
+<!--    </form>-->
+<!--    <!--    -->--><?////=$id?>
+<!--</div>-->
